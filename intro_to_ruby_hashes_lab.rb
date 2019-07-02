3e73a5d3dc9f0d1760913620c05253bc72543162
Andnrew Cheon <cheonandrew113@gmail.com>
2019-07-02 00:41:39 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -32,9 +32,9 @@ def update_counting_hash(hash, key)
   # given a hash an a key as parameters, return an updated hash
   # if the provided key is not present in the hash, add it and assign it to the value of 1
   # if the provided key is present, increment its value by 1
-    if shipping_manifest["top hat"]
-      shipping_manifest["top hat"] = 1
+    if update_counting_hash["count"]
+      update_counting_hash["count"] = 1
     else
-      shipping_manifest["top hat"] += 1
+      update_counting_hash["count"] += 1
     end
 end
